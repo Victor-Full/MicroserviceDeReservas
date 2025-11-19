8b078315df08a75637435d01367b06443673cad6
Luis Valdo <luisvso.dev@gmail.com>
2025-11-19 17:22:40 -0300

    feat(ReservaController): Adicionado o controller de listagem de espacos alocados

--- src/main/java/com/example/microservicoDeReservas/controller/ReservaController.java
+++ src/main/java/com/example/microservicoDeReservas/controller/ReservaController.java
@@ -19,34 +19,38 @@ public class ReservaController {
     }
 
     @PostMapping
-    public ResponseEntity<Reserva> criar(@RequestBody ReservaDTO dto){
+    public ResponseEntity<Reserva> criar(@RequestBody ReservaDTO dto) {
         return ResponseEntity.ok(service.criar(dto));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Reserva> update(@PathVariable Long id, @RequestBody ReservaDTO dto){
+    public ResponseEntity<Reserva> update(@PathVariable Long id, @RequestBody ReservaDTO dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Reserva> findById(@PathVariable Long id){
+    public ResponseEntity<Reserva> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
     @GetMapping
-    public ResponseEntity<List<Reserva>> listar(){
-    return ResponseEntity.ok(service.listarTodas());
+    public ResponseEntity<List<Reserva>> listar() {
+        return ResponseEntity.ok(service.listarTodas());
     }
 
     @GetMapping("/espaco/{espacoId}")
-    public ResponseEntity<List<Reserva>> listarPorEspaco(@PathVariable long espacoId){
+    public ResponseEntity<List<Reserva>> listarPorEspaco(@PathVariable long espacoId) {
         return ResponseEntity.ok(service.listarPorEspaco(espacoId));
     }
-}
 
+    @GetMapping("/alocadas")
+    public ResponseEntity<List<Reserva>> listarAlocados() {
+        return ResponseEntity.ok(service.findValid());
+    }
+}
